1d5105ef7e2c7134ec16738b9fc11a6f55716f2c
Bas Pijls <b.pijls@hva.nl>
2026-04-23 10:52:21 +0200

    Refactor sketch.js to use dynamic circle positioning based on mouse coordinates

--- tiles/hans/sketch.js
+++ tiles/hans/sketch.js
@@ -1,7 +1,14 @@
+dingetje = {
+    centerX: 100,
+    centerY: 50,
+    diameter: 100
+}
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400);      
 }
 
 function draw() {
-  background(220);
+    background(220); 
+    circle(dingetje.centerX, dingetje.centerY, dingetje.diameter);
 }
