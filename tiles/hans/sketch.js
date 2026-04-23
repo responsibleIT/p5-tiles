12da23683ca70e22ea4254d7731324191e30463c
Bas Pijls <b.pijls@hva.nl>
2026-04-23 10:59:03 +0200

    Refactor sketch.js to use an array for multiple circles and improve drawing logic

--- tiles/hans/sketch.js
+++ tiles/hans/sketch.js
@@ -1,14 +1,25 @@
-dingetje = {
-    centerX: 100,
-    centerY: 50,
-    diameter: 100
-}
+let dingetjes = [];
 
 function setup() {
-    createCanvas(400, 400);      
+    createCanvas(400, 400);  
+    
+    for (let iDingetje=0; iDingetje<100; iDingetje++){
+        dingetjes.push({
+            centerX: 100,
+            centerY: 50,
+            diameter: 100
+        });
+    }
 }
 
 function draw() {
-    background(220); 
-    circle(dingetje.centerX, dingetje.centerY, dingetje.diameter);
+    background(220);
+
+    for (let iDingetje=0; iDingetje<100; iDingetje++){
+        let eenDingetje = dingetjes[iDingetje];
+        circle(eenDingetje.centerX, eenDingetje.centerY, eenDingetje.diameter);
+    }
+    
+
+    
 }
