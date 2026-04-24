10ea150b2444565ab7d6bff8bc3b04d1457b870a
Petroscheva <pjhvandegeer@gmail.com>
2026-04-24 09:30:36 +0200

    alles wat we tijdens de workshop gedaan hebben

--- tiles/johan/sketch.js
+++ tiles/johan/sketch.js
@@ -15,12 +15,17 @@ function setup() {
 
   for (let iDingetje=0; iDingetje<100; iDingetje++){
     dingetjes.push({
-      centerX: 100,
-      centerY: 50,
-      diameter: 100
+      centerX: random(0, width),
+      centerY: random(0, width),
+      diameter: random(10, 20),
+      velocityX: random(-3, 3),
+      velocityY: random(-3, 3),
+      shade: random(100, 255)
     })
   }
 
+  noStroke();
+  
   // centerX = width/2;
   // centerY = height/2;
   // diameter = width;
@@ -32,10 +37,36 @@ function setup() {
 function draw() {
   background(220);
   
-  for (let iDingetje=0; iDingetje<100; iDingetje++){
+  for (let iDingetje=0; iDingetje<dingetjes.length; iDingetje++){
     let eenDingetje = dingetjes[iDingetje];
+
+    eenDingetje.centerX += eenDingetje.velocityX;
+    eenDingetje.centerY += eenDingetje.velocityY;
+
+    if (eenDingetje.centerX > width || eenDingetje.centerX < 0){
+      eenDingetje.velocityX = -eenDingetje.velocityX;
+    }
+
+    if (eenDingetje.centerY > width || eenDingetje.centerY < 0){
+      eenDingetje.velocityY = -eenDingetje.velocityY;
+    }
+
+    eenDingetje.velocityY += 0.1;
+    fill(eenDingetje.shade);
     circle(eenDingetje.centerX, eenDingetje.centerY, eenDingetje.diameter);  
+    
+    // eenDingetje.centerX += random(-4, 4);
+    // eenDingetje.centerY += random(-4, 4);
+
+    // eenDingetje.centerX = eenDingetje.centerX + 10;
+    // eenDingetje.centerY = eenDingetje.centerY + 10;
+    // eenDingetje.centerX += 1;
+    // eenDingetje.centerY += 1;
+    // eenDingetje.centerX += random(0.5, 3.5);
+    // eenDingetje.centerY += random(0.5, 3.5);
+
   }
+
   // circle(dingetje.centerX, dingetje.centerY, dingetje.diameter)
 
   // centerX = mouseX;
